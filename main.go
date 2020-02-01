e15d540b7a48dbe5194f04ebe9b59c15dabc3bc5
Hailee Miu <hailee.miu@tyvoid.com>
2020-02-01 12:05:47 -0600

    switch to mux router

--- main.go
+++ main.go
@@ -2,13 +2,19 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
 	fmt.Println("main")
-	http.HandleFunc("/", fooBarHandler)
-	http.ListenAndServe(":8080", nil)
+	r := mux.NewRouter()
+	r.HandleFunc("/", fooBarHandler).Methods("GET")
+	if err := http.ListenAndServe(":8080", r); err != nil {
+		log.Fatal(err)
+	}
 }
 
 func fooBarHandler(w http.ResponseWriter, r *http.Request) {
